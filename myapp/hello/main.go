11b7c6f94f009ec920853c135c996fac17d11d8b
robertg231 <rgorosave231@gmail.com>
2016-03-07 18:16:20 -0800

    added test html

--- myapp/hello/main.go
+++ myapp/hello/main.go
@@ -1,14 +1,32 @@
 package hello
 
 import (
-	"fmt"
 	"net/http"
+	"html/template"
+	"log"
 )
 
+var htmlTest *template.Template
+
 func init() {
+	var err error
+
+	htmlTest, err = template.ParseFiles("test.html", "test2.html")
+
+	if(err != nil){
+		log.Panic(err)
+	}
+
 	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "What is up")
+	var err error
+
+	err = htmlTest.Execute(w,nil)
+	if(err != nil){
+		log.Panic(err)
+	}
+
+
 }
