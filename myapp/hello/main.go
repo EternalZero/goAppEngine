86346ac2de2ea1e62bb03edcc54d661cd1556ebd
robertg231 <rgorosave231@gmail.com>
2016-03-07 19:40:39 -0800

    finished step 1

--- myapp/hello/main.go
+++ myapp/hello/main.go
@@ -1,3 +1,4 @@
+//package main
 package hello
 
 import (
@@ -7,17 +8,26 @@ import (
 )
 
 var htmlTest *template.Template
-
+var htmlTest2 *template.Template
 func init() {
+//func main(){
 	var err error
 
-	htmlTest, err = template.ParseFiles("test.html", "test2.html")
+	htmlTest, err = template.ParseFiles("test.html")
 
 	if(err != nil){
 		log.Panic(err)
 	}
 
+	htmlTest2, err = template.ParseFiles("test2.html")
+	if(err != nil){
+		log.Panic(err)
+	}
+
+
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/test2.html", doIcare)
+	http.ListenAndServe(":9090", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +37,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if(err != nil){
 		log.Panic(err)
 	}
+}
 
+func doIcare(w http.ResponseWriter, r * http.Request){
+	var err error
 
+	htmlTest2.Execute(w,nil)
+	if(err != nil){
+		log.Panic(err)
+	}
 }
