137a2a84ee243cf277619e014061cf471cb806da
robertg231 <rgorosave231@gmail.com>
2016-03-09 18:34:27 -0800

    cookie still not setting

--- myapp/hello/main.go
+++ myapp/hello/main.go
@@ -9,7 +9,7 @@ import (
 	//"strings"
 	"encoding/base64"
 	"encoding/json"
-	//"fmt"
+	"fmt"
 )
 
 type user struct{
@@ -35,14 +35,47 @@ func main(){
 	}
 
 
-	http.HandleFunc("/", UUIDCookie)
-
-	http.HandleFunc("/login/", handler)
-	http.HandleFunc("/login/postlogin.html", postLogin)
+	http.HandleFunc("/", getInfo)
+	http.HandleFunc("/postlogin.html", postLogin)
 	http.ListenAndServe(":9090", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+
+func postLogin(w http.ResponseWriter, r *http.Request){
+
+	var err error
+
+	//checking if cookie exists
+	cookie, err := r.Cookie("session-fino")
+
+	//if the cookie does not exist we make a new and assign it a UUID using the imported gouuid thingy
+	if(err!=nil){
+		fmt.Println("im making a cookie")
+		//we ignore the error that the NewV4 function returns. we make a randomly generated UUID
+		uuid, _ := uuid.NewV4()
+
+		//we make a new cookie using a composite literal and give its address to a pointer
+		cookie = &http.Cookie{
+			Name: "session-fino",
+			Value: uuid.String() + "|" + marshallUserType(r),
+			HttpOnly: true,
+			//can't use secure cause we don't have https available
+			// /Secure: true,
+		}
+
+	fmt.Println(cookie)
+	}
+	http.SetCookie(w, cookie)
+	//we set the cookie on the users pc
+
+	err = htmlTest2.Execute(w,nil)
+	if(err != nil){
+		//log.Panic(err)
+	}
+}
+
+func getInfo(w http.ResponseWriter, r * http.Request){
+
 	var err error
 
 	err = htmlTest.Execute(w,nil)
@@ -51,20 +84,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func postLogin(w http.ResponseWriter, r *http.Request){
-	var err error
-
-	cookie, err := r.Cookie("session-fino")
-
-	if(err == http.ErrNoCookie){
-		http.Redirect(w,r,"/login/",303)
-		return
-	}
-
-	htmlTest2.Execute(w,nil)
-	if(err != nil){
-		log.Panic(err)
-	}
+func marshallUserType(r * http.Request) string{
 
 	currUser := user{
 		Name: r.FormValue("name"),
@@ -78,48 +98,5 @@ func postLogin(w http.ResponseWriter, r *http.Request){
 
 	userJSONString := base64.StdEncoding.EncodeToString(userJSON)
 
-	cookie.Value = cookie.Value + "|" + userJSONString
-
-
-	newCookie := &http.Cookie{
-		Name: "session-fino",
-		Value: cookie.Value,
-		HttpOnly: true,
-		//Secure: true
-	}
-	//the values are correct everything gets converted ok
-	//but i Cant set the cookie for some reason :((((((((
-	//if i just update the original cookie it doesn't set
-	//my new cookie is not being set
-	//why Can't i set these cookies......
-	http.SetCookie(w, newCookie)
-	//fmt.Fprintln(w, cookie)
-
-}
-
-func UUIDCookie(w http.ResponseWriter, r * http.Request){
-
-
-	//checking if cookie UUID exists
-	cookie, err := r.Cookie("session-fino")
-
-	//if the cookie does not exist we make a new and assign it a UUID using the imported gouuid thingy
-	if(err == http.ErrNoCookie){
-		//we ignore the error that the NewV4 function returns. we make a randomly generated UUID
-		uuid, _ := uuid.NewV4()
-
-		//we make a new cookie using a composite literal and give its address to a pointer
-		cookie = &http.Cookie{
-			Name: "session-fino",
-			Value: uuid.String(),
-			HttpOnly: true,
-			//can't use secure cause we don't have https available
-			// /Secure: true,
-		}
-
-		//we set the cookie on the users pc
-		http.SetCookie(w, cookie)
-	}
-
-	http.Redirect(w,r,"/login/", 303)
+	return userJSONString
 }
